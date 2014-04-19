0da43d27089577dd32d6da2bfa90337204894d3f
Brandon Li <BrandonLi622@github.com>
2014-04-19 14:16:20 -0400

    Added UI spruce-up, AJAX, links to account, user photos

--- agent/urls.py
+++ agent/urls.py
@@ -13,5 +13,6 @@ urlpatterns = patterns('',
     url(r'^agent/', search),
     url(r'^home/(?P<accessToken>\w*)/$', logged_in),
     url(r'^test/', tests),
+    url(r'^ajax_search/', ajax_search)
     #url(r'^w*', bad_address), #matches everything else
 )
